34dfe9b40b4b4b925bcdc4237a9ee67c599f38a4
k3marifn <devontaytayvon@gmail.com>
2025-02-12 15:42:22 -0500

    Add files via upload

---
+++ throwup.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Throwup
+// @namespace    https://discord.gg/TDX7hEpPH4
+// @icon         https://login.i-ready.com/favicon.ico
+// @version      v1.6
+// @description  Throwup Iready client
+// @author       Throwup Team
+// @match        https://login.i-ready.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @run-at       document-start
+// @updateURL    https://github.com/k3marifn/throwup/raw/refs/heads/main/throwup.user.js
+// @downloadURL  https://github.com/k3marifn/throwup/raw/refs/heads/main/throwup.user.js
+// @require      https://pastebin.com/raw/yUpbgyrz
+// ==/UserScript==
+
+/*
+ only the throwups
+*/
